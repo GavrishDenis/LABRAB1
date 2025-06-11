02a2cca210c33b39e8a364f4dabee06dcc85beac
GavrishDenis <denis.bobrboy@gmail.com>
2025-06-11 10:54:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Явно указываем Axios как внешнюю зависимость
+      output: {
+        manualChunks: {
+          axios: ['axios'], // Создаем отдельный бандл для Axios
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['axios'], // Добавляем Axios в оптимизацию зависимостей
+  },
+});
